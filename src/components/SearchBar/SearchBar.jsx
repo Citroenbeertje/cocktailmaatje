853c7e8f1f1dcedecd61ebeeb5f1fdfdfdfdad69
Citroenbeertje <sportsproet@gmail.com>
2024-04-23 19:00:48 +0200

    AZList in Searchbar gezet

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,15 +1,16 @@
 import './SearchBar.css'
 import axios from 'axios';
+import AZList from "../AZList/AZList.jsx";
 
 // List all cocktails by first letter
 // www.thecocktaildb.com/api/json/v1/1/search.php?f=a
 
 function SearchBar({placeholder, data}) {
 
-    const handleClick = (letter) => {
-        console.log(letter);
-        // TODO letter nog meegeven axios om cocktail lijst te tonen
-    }
+    // const handleClick = (letter) => {
+    //     console.log(letter);
+    //     // TODO letter nog meegeven axios om cocktail lijst te tonen
+    // }
 
     return (
         <div className="search">
@@ -22,11 +23,12 @@ function SearchBar({placeholder, data}) {
                     <div className="search-icon">searchicon</div>
                     <button>Search</button>
                 </div>
-                <div className='a-z-list'>{"A B C D E F G H I J K L M N O P Q R S T U V W".split(" ").map((letter) => (
-                    <div key={letter} onClick={() => handleClick(letter)}>
-                        {letter}
-                    </div>
-                ))}</div>
+                <AZList/>
+                {/*<div className='a-z-list'>{"A B C D E F G H I J K L M N O P Q R S T U V W".split(" ").map((letter) => (*/}
+                {/*    <div key={letter} onClick={() => handleClick(letter)}>*/}
+                {/*        {letter}*/}
+                {/*    </div>*/}
+                {/*))}</div>*/}
 
             </div>
 
