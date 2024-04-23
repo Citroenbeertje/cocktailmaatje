2a7d0801dc65a465117ccff9d6e4b7a5444b46f3
Citroenbeertje <sportsproet@gmail.com>
2024-04-23 08:06:45 +0200

    opzetje CocktailCard

---
+++ src/components/CocktailCard/CocktailCard.jsx
@@ -0,0 +1,13 @@
+function CocktailCard({strDrink, strIngredient1}) {
+
+    return (
+    <div>
+        <div>Title is {strDrink}</div>
+        <div>MainIngredient is {strIngredient1}</div>
+    </div>
+    );
+}
+
+<CocktailCard strDrink={} strIngredient1={}/>
+
+export default CocktailCard;
