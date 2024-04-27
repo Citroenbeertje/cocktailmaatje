e10d83d49fe527c8e6bb2232c31917bf9f365442
Citroenbeertje <sportsproet@gmail.com>
2024-04-27 13:31:38 +0200

    secondary dropdown haalt api ingredient list op

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,24 +1,36 @@
 import './SearchBar.css';
 import axios from 'axios';
 import AZList from "../AZList/AZList.jsx";
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 // import icon from '../../assets/glass-icon.png';
 
 // List all cocktails by first letter
 // www.thecocktaildb.com/api/json/v1/1/search.php?f=a
 
 function SearchBar({placeholder= "Search for cocktails", data}) {
-
-    const [filterType, setFilterType] = useState("search all")
-
-
-    // console.log(filterType)
-
+    const [filterType, setFilterType] = useState("search all");
+    const [ingredientOptions, setIngredientOptions] = useState([]);
+    console.log("ingredientOptions", ingredientOptions)
     const handleClick = () => {
-    //     console.log('search');
-    //     // TODO letter nog meegeven axios om cocktail lijst te tonen
     };
 
+
+    //
+    useEffect(() => {
+        const fetchIngredients = async () => {
+            const response = await axios.get('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list');
+            console.log("response", response);
+            setIngredientOptions(response.data.drinks);
+        };
+
+        fetchIngredients();
+        console.log("ingredientOptions", ingredientOptions);
+    }, [])
+    // // console.log(filterType)
+    //
+
+    // --renderbar functie --
+    // rendered conditioneel het tweede deel van de zoekfunctie
     const renderBar = (filterTypeValue) => {
         console.log("filterTypeValue", filterTypeValue)
         if (filterTypeValue === "cocktails" || filterTypeValue === "search all") {
@@ -37,8 +49,10 @@ function SearchBar({placeholder= "Search for cocktails", data}) {
                 <select
                     name="secondary-dropdown"
                     id="blabla">
+                    {ingredientOptions.map((ingredient) => {
+                        return <option value={ingredient.strIngredient1}>{ingredient.strIngredient1}</option>
+                    })}
 
-                    <option value="ingredient">---- Choose Ingredient ----</option>
                 </select>
             </>
         }
