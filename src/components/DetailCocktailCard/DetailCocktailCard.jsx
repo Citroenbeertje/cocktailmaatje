9158a8fff3056d3cb01afc3d6180eb74ea00d8bd
Citroenbeertje <sportsproet@gmail.com>
2024-05-01 17:28:59 +0200

    cocktailkaartjes styling gedaan

--- src/components/DetailCocktailCard/DetailCocktailCard.jsx
+++ src/components/DetailCocktailCard/DetailCocktailCard.jsx
@@ -1,25 +1,19 @@
 import './DetailCocktailCard.css'
 function DetailCocktailCard(drinks) {
     return (
-        <div className="parent-container">
             <div className="cocktail-card">
-                <div className="image-side">
                     <img className="detail-image"
                         src="https://cocktail-stack.netlify.app/static/3b40847ea8709ee1224bef4e95b6485d/9a128/k6v97f1487602550.jpg"
                         alt="Cocktail Image"
                     />
-                </div>
                 <div className="information-side">
                     <div className="title-detail-card">Grizzly Bear{drinks.strDrink}</div>
                     <div className="">main ingredient{drinks.strIngredient1}</div>
                     <div className="">Category drink{drinks.strCategory}</div>
-                    <div className="">
                         <span>non-alcoholic{drinks.strAlcoholic}</span>
                         <span>glass shape{drinks.strGlass}</span>
-                    </div>
                 </div>
             </div>
-        </div>
     );
 }
 
--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,6 +1,7 @@
 
-import './BigCocktailCard.css'
-function BigCocktailCard() {
+import './RecipeCard.css'
+import DetailCocktailCard from "../DetailCocktailCard/DetailCocktailCard.jsx";
+function RecipeCard() {
     const drinks = {
         strDrink: 'Grizzly Bear',
         strIngredient1: 'Main Ingredient',
@@ -11,32 +12,14 @@ function BigCocktailCard() {
 
     return (
         <div className="recipe-card-parent">
-            <div className="title-recipecard">{drinks.strDrink}</div>
-            <div className="big-card">
-                <div className="recipe-image-side">
-                    <img className="big-image"
-                        src="https://cocktail-stack.netlify.app/static/3b40847ea8709ee1224bef4e95b6485d/9a128/k6v97f1487602550.jpg"
-                        alt="Cocktail Image"
-                    />
-                </div>
-                <div className="recipe-specs">
-                    <div className="">{drinks.strIngredient1}</div>
-                    <div className="">{drinks.strCategory}</div>
-                    <div className="">{drinks.strAlcoholic}</div>
-                    <div className="">{drinks.strGlass}</div>
-                </div>
-            </div>
-            <div className="recipe-text-block">
+            <DetailCocktailCard/>
                 <div className="recipe-text-measurement">
-                    Measure
-                </div>
+                    Measure</div>
                 <div className="recipe-text-instructions">
-                    Pour all ingredients into a cocktail shaker, mix and serve over ice into a chilled glass.
-                </div>
-            </div>
+                    Pour all ingredients into a cocktail shaker, mix and serve over ice into a chilled glass.</div>
         </div>
 
     );
 }
 
-export default BigCocktailCard;
+export default RecipeCard;
