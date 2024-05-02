9afb57359bc8468313c843f8e71b03983bebfaeb
Citroenbeertje <sportsproet@gmail.com>
2024-05-02 17:07:26 +0200

    Styling van Form gedaan en opzetje voor nieuwe Form component

---
+++ src/components/Form/LoginForm.jsx
@@ -0,0 +1,9 @@
+import LoginRegisterForm from "./LoginRegisterForm.jsx";
+import { useForm } from 'react-hook-form';
+import Button from "../Button/Button.jsx";
+import React from "react";
+import "./LoginRegisterForm.css"
+import TextField from "./TextField.jsx";
+import PasswordField from "./PasswordField.jsx";
+
+export default LoginForm;
