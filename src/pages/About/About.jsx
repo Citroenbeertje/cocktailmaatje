7cacf1f64791b462f369e53875e6c0b1dc85bc54
Citroenbeertje <sportsproet@gmail.com>
2024-05-25 11:24:56 +0200

    inhoud about pagina + css van clicked heart verbeterd

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,8 +2,21 @@ function About() {
     return (
         <div className="">
             <h1>About</h1>
+            <h2>Why Cocktailmaatje?</h2>
+            <p>With so many cocktails available, it’s easy to feel overwhelmed. You might end up making the same cocktails over and over due to decision fatigue. The goal of Cocktailmaatje is to make it easy for you to discover new cocktails.</p>
+            <h2>How to use this application?</h2>
+            <p>You can search for cocktails in various ways. </p>
+            <ul>
+                <li>Whether it’s by first letter by selecting that letter in de A to Z list under de search bar</li>
+                <li>type in a specific string of character in the search bar</li>
+                <li>ingredient</li>
+                <li>the drink category such as cocktails, party drinks or shots</li>
+                <li>the type of glass used</li>
+                <li>filter on non-alcoholic or optionally alcoholic drinks</li>
+            </ul>
+            <p>Cocktailmaatje has your back.</p>
         </div>
-    )
+)
 }
 
 export default About;
