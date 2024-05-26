fe3a35545e78f17eb548168843ebda126e206ea9
Citroenbeertje <sportsproet@gmail.com>
2024-05-26 21:05:53 +0200

    Missende data op DetailCocktailCard wordt nu getoond

--- src/components/DetailCocktailCard/DetailCocktailCard.jsx
+++ src/components/DetailCocktailCard/DetailCocktailCard.jsx
@@ -1,27 +1,35 @@
 import './DetailCocktailCard.css'
 import ClickedHeart from "../ClickedHeart/ClickedHeart.jsx";
+import {useEffect, useState} from "react";
+import axios from "axios";
 function DetailCocktailCard({cocktail, userIsLoggedIn, username, JWTToken, favorites, setFavorites }) {
+    const [cocktailInfo, setCocktailInfo] = useState({})
+
+    useEffect(() => {
+        const fetchDrinkInformation = async () => {
+            const response = await axios.get(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktail.idDrink}`);
+            console.log("response", response)
+            setCocktailInfo(response.data.drinks[0])
+        }
+
+        fetchDrinkInformation();
+    }, [cocktail.idDrink])
 
     return (
             <div className="detail-cocktail-card">
                 <div className="image-container">
                     <img className="detailcard-image"
-                        src={cocktail.strDrinkThumb}
+                        src={cocktailInfo.strDrinkThumb}
                         alt="Cocktail Image"
                     />
                 </div>
                 <div className="information-side">
-                    {/*<div className="container-title-heart">*/}
-                    {/*    <div className="title-detail-card">{cocktail.strDrink}</div>*/}
-                    {/*    <div className="heart-container"><ClickedHeart/></div>*/}
-
-                    {/*</div>*/}
-                    <div className="title-detail-card">{cocktail.strDrink}</div>
-                    <div className="main-ingredient">{cocktail.strIngredient1}</div>
-                    <div className="">Category:{cocktail.strCategory}</div>
-                    <div className="">{cocktail.strAlcoholic}</div>
-                    <div className="">Glass: {cocktail.strGlass}</div>
-                    <div className="heart"><ClickedHeart cocktailID={cocktail.idDrink} cocktailName={cocktail.strDrink} userIsLoggedIn={userIsLoggedIn} username={username} JWTToken={JWTToken} favorites={favorites} setFavorites={setFavorites} /></div>
+                    <div className="title-detail-card">{cocktailInfo.strDrink}</div>
+                    <div className="main-ingredient">{cocktailInfo.strIngredient1}</div>
+                    <div className="">Category:{cocktailInfo.strCategory}</div>
+                    <div className="">{cocktailInfo.strAlcoholic}</div>
+                    <div className="">Glass: {cocktailInfo.strGlass}</div>
+                    <div className="heart"><ClickedHeart cocktailID={cocktailInfo.idDrink} cocktailName={cocktailInfo.strDrink} userIsLoggedIn={userIsLoggedIn} username={username} JWTToken={JWTToken} favorites={favorites} setFavorites={setFavorites} /></div>
                 </div>
             </div>
     );
